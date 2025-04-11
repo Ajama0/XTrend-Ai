052117dd5dffe634f1c7a1fb67bc167a2917bfd3
ajama0 <abasjama04@hotmail.com>
2025-04-11 21:00:55 +0300

    initial structure

---
+++ Backend/Xtrend-Ai/src/main/java/com/example/Xtrend_Ai/XtrendAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Xtrend_Ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtrendAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtrendAiApplication.class, args);
+	}
+
+}
---
+++ Backend/Xtrend-Ai/src/test/java/com/example/Xtrend_Ai/XtrendAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Xtrend_Ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtrendAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
