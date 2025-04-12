43a6ba8dc18f733d388b6c8ff11e13273c5bb0e6
ajama0 <abasjama04@hotmail.com>
2025-04-12 19:08:52 +0300

    add landing page ui, and handle login validation

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { validate } from 'json-schema';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
+  standalone:true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
   
-  
+  loginForm!:FormGroup
   ngOnInit(): void {
 
     //this runs as soon as the component is instatiated and the constructor is resolved
     this.testerMethod()
+    this.formStructure()
   }
 
-  constructor(private user:UserService){}
+  constructor(private user:UserService, private formBuilder:FormBuilder, private router:Router){}
 
 
   testerMethod(){
@@ -32,6 +39,39 @@ export class LoginComponent implements OnInit{
     })
 
   }
+
+  formStructure(){
+    this.loginForm = this.formBuilder.group({
+      'firstname' : ['', Validators.required],
+      'lastname' : ['', Validators.required],
+      'email' : ['', Validators.required],
+      'password' : ['', Validators.required],
+       //the interests part will be a checkbox which defines the users interests (only present in the signup section)
+    })
+
+  }
+
+  //on form submission this is called. 
+  validateForm(form:FormGroup){
+   if(form.controls['email'].value === 'harry@example.com' && form.controls['password'].value === 'harry1234'){
+    ///user will be routed to the homepage in which they can be begin creating content!
+    console.log('successfull login')
+    this.router.navigate(['/dashboard'])
+   }
+    else{
+      throw new Error('please use the correct credentials')
+    }
+    
+  }
+
+
+
+
+  
+
+
+
+  
 
 
 
