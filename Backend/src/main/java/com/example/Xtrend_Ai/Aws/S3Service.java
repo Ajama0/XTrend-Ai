cb3b631aeea88eea85b6ec6099df605db7a6d035
ajama0 <abasjama04@hotmail.com>
2025-07-19 14:57:45 +0300

    s3 service complete

--- Backend/src/main/java/com/example/Xtrend_Ai/Aws/S3Service.java
+++ Backend/src/main/java/com/example/Xtrend_Ai/Aws/S3Service.java
@@ -2,17 +2,26 @@ package com.example.Xtrend_Ai.Aws;
 
 
 import lombok.RequiredArgsConstructor;
-import okhttp3.Request;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+import java.net.URL;
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
 
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
 
 
     /***
@@ -30,6 +39,66 @@ public class S3Service {
         s3Client.putObject(objectRequest, RequestBody.fromBytes(audio));
 
     }
+
+
+
+    public GetObjectRequest getObject(String bucket, String key) {
+        return GetObjectRequest
+                .builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        //this is our object that we use retrieve the file by passing in our bucket and key
+    }
+
+    /**
+     *
+     * @param bucket - location of stored objects
+     * @param key - audio byte location
+     * - use Presigned urls which are generated from aws that allow users to access the audio stream directly from our bucket
+     * - however the credentials to our bucket are hidden
+     * - instead of returning bytes from the image recieved from AWS..
+     * every time a user makes a request we generate a signed url
+     * url expiration time is set to 7 days
+     *
+     * @return presigned URL
+     */
+
+    public URL generatePreSignedUrls(String bucket, String key) {
+
+        GetObjectRequest objectRequest = getObject(bucket, key);
+        // you can change expiration time here
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(3600 * 7)) //24 hours
+                .getObjectRequest(objectRequest)
+                .build();
+
+        //the presigned bean, uses an implementation to generate a url for our object request
+        //this returns us a presigned object based on our request which includes the actual file and the expiration
+        PresignedGetObjectRequest presigned = s3Presigner.presignGetObject(presignRequest);
+        return presigned.url();
+
+    }
+
+
+    public void DeleteObject(String bucket, String key) {
+
+        try{
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .build();
+            s3Client.deleteObject(deleteObjectRequest);
+
+        }catch(AwsServiceException awsServiceException){
+            throw new RuntimeException("error occurred whilst deleting object", awsServiceException);
+        }
+
+
+    }
+
+
 
 
 
