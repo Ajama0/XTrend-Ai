0b9039b29b8292f1023aa9cb5f1cf54b48619fd6
ajama0 <abasjama04@hotmail.com>
2025-07-26 20:23:17 +0300

    integration testing for s3 service complete

--- Backend/src/test/java/com/example/Xtrend_Ai/Aws/S3ServiceTest.java
+++ Backend/src/test/java/com/example/Xtrend_Ai/Aws/S3ServiceTest.java
@@ -7,13 +7,21 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import retrofit2.http.Url;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 class S3ServiceTest {
@@ -68,10 +76,44 @@ class S3ServiceTest {
     }
 
     @Test
-    void getPresignedForObject() {
+    void getPresignedForObject() throws MalformedURLException {
+        String bucketName = "testbucket";
+        String objectKey = "testobject";
+        URL url = new URL("http://example.com");
+
+
+        //mock response object to access call another function which is the url
+        PresignedGetObjectRequest presignedGetObjectRequest = mock(PresignedGetObjectRequest.class);
+        when(presignedGetObjectRequest.url()).thenReturn(url);
+        //whenever we call the function return the mocked presignedgetobject class
+        when(s3Presigner.presignGetObject(any(GetObjectPresignRequest.class))).thenReturn(presignedGetObjectRequest);
+
+
+        //when
+        URL presignedURL = underTest.getPresignedForObject(bucketName, objectKey);
+        ArgumentCaptor<GetObjectPresignRequest> presignRequestArgumentCaptor = ArgumentCaptor.forClass(GetObjectPresignRequest.class);
+
+        //then
+        verify(s3Presigner).presignGetObject(presignRequestArgumentCaptor.capture());
+
+        assertEquals(bucketName,presignRequestArgumentCaptor.getValue().getObjectRequest().bucket());
+        assertEquals(objectKey,presignRequestArgumentCaptor.getValue().getObjectRequest().key());
+        assertEquals(presignedURL,url);
+
     }
 
     @Test
     void deleteObject() {
+
+        //given
+        String bucketName = "testbucket";
+        String objectKey = "testobject";
+
+        //when
+        assertDoesNotThrow(()->{
+            underTest.DeleteObject(bucketName, objectKey);
+            verify(s3Client).deleteObject(any(DeleteObjectRequest.class));
+        });
+
     }
 }
